7583b1367a1b83307bc5089ffb9fc67ae9d894ae
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2025-01-27 00:25:18 +0530

    prod issues

--- components/ui/SearchResultsClient.tsx
+++ components/ui/SearchResultsClient.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import { useRouter } from "next/navigation";
 import GameCard from "@/components/ui/GameCard";
 import { useLoading } from "@/context/LoadingContext";
 
